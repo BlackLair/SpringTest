a2a52166cfaa772c08fa791e25f6eafbc0df2688
JW <kiw970923@naver.com>
2024-02-21 16:07:41 +0900

    [FIX] MyBatis 연습문제1  - Controller 메소드 매개인자 어노테이션 수정

--- src/main/java/com/jwtest/spring/test/mybatis/RealEstateController.java
+++ src/main/java/com/jwtest/spring/test/mybatis/RealEstateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jwtest.spring.test.mybatis.domain.RealEstate;
@@ -16,19 +17,19 @@ public class RealEstateController {
 	RealEstateService realEstateService;
 	
 	@RequestMapping("/mybatis/real-estate/1")
-	public RealEstate getRealEstate(@Param("id") int id) {
+	public RealEstate getRealEstate(@RequestParam("id") int id) {
 		RealEstate realEstate = realEstateService.getRealEstate(id);
 		return realEstate;
 	}
 	
 	@RequestMapping("/mybatis/real-estate/2")
-	public List<RealEstate> getCheapRealEstate(@Param("rent") int rent){
+	public List<RealEstate> getCheapRealEstate(@RequestParam("rent") int rent){
 		List<RealEstate> list = realEstateService.getCheapRealEstateList(rent);
 		return list;
 	}
 	
 	@RequestMapping("/mybatis/real-estate/3")
-	public List<RealEstate> getGoodRealEstate(@Param("area") int area, @Param("price") int price){
+	public List<RealEstate> getGoodRealEstate(@RequestParam("area") int area, @RequestParam("price") int price){
 		List<RealEstate> list = realEstateService.getGoodRealEstateList(area, price);
 		return list;
 		
