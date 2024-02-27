96bdba18b3aaf8e43d086bbcb17e9837ad7ba3c9
JW <kiw970923@naver.com>
2024-02-27 17:48:41 +0900

    [ADD] JSTL 연습문제1  - JSTL core 실습

---
+++ src/main/java/com/jwtest/spring/test/jstl/JSTLController.java
@@ -0,0 +1,13 @@
+package com.jwtest.spring.test.jstl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JSTLController {
+	
+	@GetMapping("/jstl/test01")
+	public String Test01() {
+		return "/jstl/test01";
+	}
+}
